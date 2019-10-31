eaa56c9911175e31bdfc389bdddf47ade941430e
ks147 <kartiks31416@gmail.com>
2019-10-31 22:08:28 +0530

    Lab 9 Q2 (b) done

--- Lab9/lab9_q2.m
+++ Lab9/lab9_q2.m
@@ -1,34 +1,72 @@
+%Equation of the form -y'' + p(x)y' + q(x)y = r(x)
+%(a)
+p = @(x) -x;
+q = @(x) 2;
+r = @(x) -2 -(2+x.^2)*exp(x);
+a = -1;
+b = 0;
+alpha = -2;
+beta = 1;
+h = 1/400;
+exact_sol = @(x) x.^2 + x.*exp(x);
+fprintf('Q2(a)\n');
+y = second_orderQ2(a,b,h,alpha,beta,p,q,r);
+plot_error([a:h:b],y,exact_sol);
 
-function y = forward_diff(a,b,h,alpha,beta,p,q,r,fig_no)
+%(b)
+p = @(x) 0;
+q = @(x) -x;
+r = @(x) (-3+x+x.^2 - x.^3).*sin(x) - 4*x.*cos(x);
+h = 1/400;
+a = 0;
+b = 1;
+alpha = -1;
+beta = 2*sin(1);
+exact_sol = @(x) (x.^2 + x.*exp(x));
+fprintf('Q2(b)\n');
+y = second_orderQ2(a,b,h,alpha,beta,p,q,r);
+
+
+
+
+
+
+function y = second_orderQ2(a,b,h,alpha,beta,p,q,r)
 t = [a:h:b];
 n = length(t);
-y = zeros(1,n);
-y(1) = alpha;
-y(n) = beta;
 
-A = zeros(n-2,n-2);
-b = zeros(n-2,1);
-% i = 2
-A(1,1) = (2/h^2 - p(t(2))/h + q(t(2)));
-A(1,2) = (-1/h + p(t(2)))/h;
-b(1) = r(t(2)) - y(1)*(-1/h^2);
-%i = n
-A(n-2,n-3) = -1/h^2;
-A(n-2,n-2) = (2/h^2 - p(t(n-1))/h + q(t(n-1)));
-b(n-2) = r(t(n-1)) - y(n)*(-1/h + p(t(n-1)))/h;
+%Hard coded boundary condition for Q1
+A(1,1) = -1/h;
+A(1,2) = 1/h;
+b(1) = alpha;
+A(n,n) = 1/h;
+A(n,n-1) = -1/h;
+b(n,1) = beta;
 
-for i=3:n-2
-    A(i-1,i-2) =  -1/h^2;
-    A(i-1,i-1) = (2/h^2 - p(t(i))/h + q(t(i)));
-    A(i-1,i) = (-1/h + p(t(i)))/h;
-    b(i-1) = r(t(i));
+for i=2:n-1
+    A(i,i-1) =  -1/h^2 - p(t(i))/(2*h);
+    A(i,i) = (2/h^2 + q(t(i)));
+    A(i,i+1) = (-1/h + p(t(i))/2)/h;
+    b(i) = r(t(i));
 end
-y(2:n-1) = A\b;
-figure(fig_no);
+
+y = A\b;
+figure;
 plot(t,y);
 xlabel('x');
 ylabel('y(x)');
-title('Forward Difference');
+title('Second Order Solution');
+
+end
+
+function plot_error(x,y,exact_sol)
+error = abs(exact_sol(x) - y');
+%plot
+figure;
+plot(x,error);
+xlabel('x');
+ylabel('y(x)');
+title('Error');
 
 end
 
